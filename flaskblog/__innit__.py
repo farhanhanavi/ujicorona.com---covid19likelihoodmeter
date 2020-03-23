6717719767b7b2859b53760eabbf385197da26fb
Diva Alwi Tama <divaalwi08@gmail.com>
2020-03-23 19:30:42 +0700

    Add Validation
    Adding numeric validation in registration form

--- flaskblog/__innit__.py
+++ flaskblog/__innit__.py
@@ -6,6 +6,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'a93kfialeofp0283ixmb7fj3kgyth27f'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost:3306/sikat_aja'
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -15,5 +16,10 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
+# #create all db tables
+# @app.before_first_request
+# def create_tables():
+#     from flaskblog.models import User, ContactHistory, Symptoms
+#     db.create_all()
 
 from flaskblog import route
